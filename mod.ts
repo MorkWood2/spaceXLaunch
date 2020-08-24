58a5c70da4da7b32c12ec666632e0b3d0aa435ef
Mark Wild <mark.wild189@gmail.com>
2020-08-24 14:54:48 -0700

    commit

--- mod.ts
+++ mod.ts
@@ -1,30 +1,38 @@
-import * as log from "https://deno.land/std@0.66.0/log/mod.ts";
-//defined interface
+import * as log from "https://deno.land/std/log/mod.ts";
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
 interface Launch {
   flightNumber: number;
   mission: string;
+  rocket: string;
+  customers: Array<string>;
 }
-//              pass in  a number and mapping it to launch
+
 const launches = new Map<number, Launch>();
 
-async function downLoadLaunchData() {
+async function downloadLaunchData() {
   log.info("Downloading launch data...");
-  const response = await fetch(
-    "https://api.spacexdata.com/v4/launches/latest",
-    {
-      method: "GET",
-    },
-  );
+  const response = await fetch("https://api.spacexdata.com/v3/launches", {
+    method: "GET",
+  });
 
   if (!response.ok) {
-    log.warning("Problem downloading launch data ");
+    log.warning("Problem downloading launch data.");
     throw new Error("Launch data download failed.");
   }
+
   const launchData = await response.json();
   for (const launch of launchData) {
+    const payloads = launch["rocket"]["second_stage"]["payloads"];
+    const customers = _.flatMap(payloads, (payload: any) => {
+      return payload["customers"];
+    });
+
     const flightData = {
       flightNumber: launch["flight_number"],
       mission: launch["mission_name"],
+      rocket: launch["rocket"]["rocket_name"],
+      customers,
     };
 
     launches.set(flightData.flightNumber, flightData);
@@ -33,4 +41,7 @@ async function downLoadLaunchData() {
   }
 }
 
-downLoadLaunchData();
+if (import.meta.main) {
+  await downloadLaunchData();
+  log.info(`Downloaded data for ${launches.size} SpaceX launches.`);
+}
